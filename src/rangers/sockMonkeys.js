42d019af01e4a339c388c66ed6f8e1679b681ed5
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-08-15 22:43:59 -0400

    added more arrays

---
+++ src/rangers/sockMonkeys.js
@@ -0,0 +1,32 @@
+const sockMonkeys = [
+    {
+        id: 0,
+        name: "",
+        color: ""
+    },
+    {
+        id: 1,
+        name: "",
+        color: ""
+    },
+    {
+        id: 2,
+        name: "",
+        color: ""
+    },
+    {
+        id: 3,
+        name: "",
+        color: ""
+    },
+    {
+        id: 4,
+        name: "",
+        color: ""
+    },
+    {
+        id: 5,
+        name: "",
+        color: ""
+    },
+]
