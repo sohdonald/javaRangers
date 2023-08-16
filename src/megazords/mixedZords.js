f27d8fe41911e44b1249dda6247d27e2ee20b827
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-08-15 23:27:07 -0400

    adding zord pseudocode

--- src/megazords/mixedZords.js
+++ src/megazords/mixedZords.js
@@ -1,30 +1,34 @@
 const mixedZords = [
-    {
-        id:"",
-        name:"",
-        specialAttack: "",
-        victoryPose: "",
-        summonBy: ""
+  {
+    id: "",
+    name: "",
+    specialAttack: "",
+    victoryPose: "",
+    summonBy: {
+      /* 3 coconutAvengers, 2 sockMonkeys*/
     },
-    {
-        id:"",
-        name:"",
-        specialAttack: "",
-        victoryPose: "",
-        summonBy: ""
+  },
+  {
+    id: "",
+    name: "",
+    specialAttack: "",
+    victoryPose: "",
+    summonBy: {
+      /* 2 coconutAvengers, 1 sockMonkeys, 1 toeNailClipper*/
     },
-    {
-        id:"",
-        name:"",
-        specialAttack: "",
-        victoryPose: "",
-        summonBy: ""
-    },
-    {
-        id:"",
-        name:"",
-        specialAttack: "",
-        victoryPose: "",
-        summonBy: ""
-    },
-]
+  },
+  {
+    id: "",
+    name: "",
+    specialAttack: "",
+    victoryPose: "",
+    summonBy: "",
+  },
+  {
+    id: "",
+    name: "",
+    specialAttack: "",
+    victoryPose: "",
+    summonBy: "",
+  },
+];
